2489999be136365f66ee27fc3fbf5be3a0536159
dperezs <dperezuk@gmail.com>
2018-04-03 22:34:37 +0100

    Syntax error: added missing ")"

--- aws/vpc/main.tf
+++ aws/vpc/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "self" {
   cidr_block        = "${var.cidr_block}"
   instance_tenancy  = "${var.instance_tenancy}"
-  tags              = "${merge(var.base_tags, var.vpc_tags, map("name", var.name)}"
+  tags              = "${merge(var.base_tags, var.vpc_tags, map("name", var.name))}"
 }
