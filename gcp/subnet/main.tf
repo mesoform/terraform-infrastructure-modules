2bc337a64fc7f861ab5220ed3988540be7671242
dperezs <dperezuk@gmail.com>
2018-04-04 17:58:47 +0100

    Added outputs to the gcp vpc

--- gcp/subnet/main.tf
+++ gcp/subnet/main.tf
@@ -1,16 +1,15 @@
-resource "google_compute_subnetwork" "self_external" {
+resource "google_compute_subnetwork" "self_public_google_access" {
   count = "${length(var.public_api_access_subnets) > 0 ? length(var.public_api_access_subnets) : 0}"
 
   name          = "${element(var.name, count.index)}"
   ip_cidr_range = "${var.ip_cidr_range[count.index]}"
   network       = "${var.vpc}"
-  private_ip_google_access = true
 }
 
 ##########################################################################################
 # The VMs in this subnet can access Google services without assigned external IP addresses
 ##########################################################################################
-resource "google_compute_subnetwork" "self_underlay" {
+resource "google_compute_subnetwork" "self_private_public_access" {
   count = "${length(var.private_api_access_subnets) > 0 ? length(var.private_api_access_subnets) : 0}"
 
   name          = "${element(var.name, count.index)}"
