74924f76d13509ca6bd7eeff088428d48d2b4f13
dperezs <dperezuk@gmail.com>
2018-04-04 17:12:39 +0100

    Added outputs to the aws vpc

--- gcp/subnet/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "gateway_address" {
-  value       = "${google_compute_subnetwork.self_external.gateway_address}"
-  description = "The IP address of the gateway."
-}
-
-output "self_link" {
-  value       = "${google_compute_subnetwork.self_external.self_link}"
-  description = "The URL of the created resource"
-}
