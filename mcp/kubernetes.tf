68a4536026bbb7bad4f413bc5216187e94e2fbec
Gareth Brown <gaz@gbmail.me>
2020-05-18 22:04:48 +0100

    mcp: handle overwriting keys with kube config in one file

--- mcp/kubernetes.tf
+++ mcp/kubernetes.tf
@@ -1,8 +1,18 @@
 locals {
-  kube_files = fileset(path.root, "../**/kubernetes.y{a,}ml")
-  kubernetes = {
-    for kube_file in local.kube_files:
-      basename(dirname(kube_file)) => yamldecode(file(kube_file))
+  kube_services_files = fileset(path.root, "../**/kubernetes-service.y{a,}ml")
+  kubernetes_services = {
+    for kube_file in local.kube_services_files:
+      basename(dirname(kube_file)) => {service: yamldecode(file(kube_file))}
       if !contains(split("/", kube_file), "terraform")
+  }
+  kube_deployments_files = fileset(path.root, "../**/kubernetes-deployment.y{a,}ml")
+  kubernetes_deployments = {
+    for kube_file in local.kube_deployments_files:
+      basename(dirname(kube_file)) => {deployment: yamldecode(file(kube_file))}
+      if !contains(split("/", kube_file), "terraform")
+  }
+  kubernetes = {
+    for app, config in local.kubernetes_services:
+        app => merge(config, lookup(local.kubernetes_deployments, app, {}))
   }
 }
