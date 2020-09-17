bfa1775768e2c547ea9ed3a65b8ff696b5d4a7ee
sautlan <sautlan@gmail.com>
2020-09-17 11:16:47 +0300

    count.index changed to for_each

--- k8s_adapter/mcp/terraform/kubernetes.tf
+++ k8s_adapter/mcp/terraform/kubernetes.tf
@@ -36,56 +36,56 @@ provider "kubernetes" {
 }
 
 resource "kubernetes_deployment" "self" {
-  count            = length(local.k8s_deployments)
-  wait_for_rollout = lookup(local.k8s_deployments[count.index].deployment, "wait_for_rollout", true)
+  for_each         = local.k8s_deployments
+  wait_for_rollout = lookup(each.value.deployment, "wait_for_rollout", true)
   metadata {
-    annotations   = lookup(local.k8s_deployments[count.index].deployment.metadata, "annotations", {})
-    generate_name = lookup(local.k8s_deployments[count.index].deployment.metadata, "name", null) == null ? lookup(local.k8s_deployments[count.index].deployment.metadata, "generate_name", {}) : null
-    name          = lookup(local.k8s_deployments[count.index].deployment.metadata, "name", null)
-    labels        = lookup(local.k8s_deployments[count.index].deployment.metadata, "labels", {})
-    namespace     = lookup(local.k8s_deployments[count.index].deployment.metadata, "namespace", null)
+    annotations   = lookup(each.value.deployment.metadata, "annotations", {})
+    generate_name = lookup(each.value.deployment.metadata, "name", null) == null ? lookup(each.value.deployment.metadata, "generate_name", {}) : null
+    name          = lookup(each.value.deployment.metadata, "name", null)
+    labels        = lookup(each.value.deployment.metadata, "labels", {})
+    namespace     = lookup(each.value.deployment.metadata, "namespace", null)
   }
   spec {
-    min_ready_seconds         = lookup(local.k8s_deployments[count.index].deployment.spec, "min_ready_seconds", null)
-    paused                    = lookup(local.k8s_deployments[count.index].deployment.spec, "paused", null)
-    progress_deadline_seconds = lookup(local.k8s_deployments[count.index].deployment.spec, "progress_deadline_seconds", null)
-    replicas                  = lookup(local.k8s_deployments[count.index].deployment.spec, "replicas", null)
-    revision_history_limit    = lookup(local.k8s_deployments[count.index].deployment.spec, "revision_history_limit", null)
+    min_ready_seconds         = lookup(each.value.deployment.spec, "min_ready_seconds", null)
+    paused                    = lookup(each.value.deployment.spec, "paused", null)
+    progress_deadline_seconds = lookup(each.value.deployment.spec, "progress_deadline_seconds", null)
+    replicas                  = lookup(each.value.deployment.spec, "replicas", null)
+    revision_history_limit    = lookup(each.value.deployment.spec, "revision_history_limit", null)
     dynamic "strategy" {
-      for_each = lookup(local.k8s_deployments[count.index].deployment.spec, "strategy", null) == null ? [] : [count.index]
+      for_each = lookup(each.value.deployment.spec, "strategy", null) == null ? {} : { strategy : each.value.deployment.spec.strategy }
       content {
-        type = lookup(local.k8s_deployments[count.index].deployment.spec.strategy, "type", null)
+        type = lookup(strategy.value, "type", null)
         dynamic "rolling_update" {
-          for_each = lookup(local.k8s_deployments[count.index].deployment.spec.strategy, "rolling_update", null) == null ? [] : [count.index]
+          for_each = lookup(strategy.value, "rolling_update", null) == null ? {} : { rolling_update : strategy.value.rolling_update }
           content {
-            max_surge       = local.k8s_deployments[count.index].deployment.spec.strategy.rolling_update.max_surge
-            max_unavailable = local.k8s_deployments[count.index].deployment.spec.strategy.rolling_update.max_unavailable
+            max_surge       = strategy.value.rolling_update.max_surge
+            max_unavailable = strategy.value.rolling_update.max_unavailable
           }
         }
       }
     }
     dynamic "selector" {
-      for_each = lookup(local.k8s_deployments[count.index].deployment.spec, "selector", null) == null ? [] : [count.index]
+      for_each = lookup(each.value.deployment.spec, "selector", null) == null ? {} : { selector : each.value.deployment.spec.selector }
       content {
-        match_labels = lookup(local.k8s_deployments[count.index].deployment.spec.selector, "match_labels", null)
+        match_labels = lookup(selector.value, "match_labels", null)
       }
     }
 
     template {
       metadata {
-        labels = lookup(local.k8s_deployments[count.index].deployment.spec.template.metadata, "labels", {})
+        labels = lookup(each.value.deployment.spec.template.metadata, "labels", {})
       }
       spec {
         dynamic "affinity" {
-          for_each = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec, "affinity", null) == null ? [] : [count.index]
+          for_each = lookup(each.value.deployment.spec.template.spec, "affinity", null) == null ? {} : { affinity : each.value.deployment.spec.template.spec.affinity }
           content {
             node_affinity {
               required_during_scheduling_ignored_during_execution {
                 node_selector_term {
                   match_expressions {
-                    key      = local.k8s_deployments[count.index].deployment.spec.template.spec.affinity.node_affinity.required_during_scheduling_ignored_during_execution.node_selector_term.match_expressions.key
-                    operator = local.k8s_deployments[count.index].deployment.spec.template.spec.affinity.node_affinity.required_during_scheduling_ignored_during_execution.node_selector_term.match_expressions.operator
-                    values   = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.affinity.node_affinity.required_during_scheduling_ignored_during_execution.node_selector_term.match_expressions, "values", null)
+                    key      = affinity.value.node_affinity.required_during_scheduling_ignored_during_execution.node_selector_term.match_expressions.key
+                    operator = affinity.value.node_affinity.required_during_scheduling_ignored_during_execution.node_selector_term.match_expressions.operator
+                    values   = lookup(affinity.value.node_affinity.required_during_scheduling_ignored_during_execution.node_selector_term.match_expressions, "values", null)
                   }
                 }
               }
@@ -93,58 +93,58 @@ resource "kubernetes_deployment" "self" {
           }
         }
         dynamic "container" {
-          for_each = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec, "container", null) == null ? [] : [count.index]
+          for_each = lookup(each.value.deployment.spec.template.spec, "container", null) == null ? {} : { container : each.value.deployment.spec.template.spec.container }
           content {
-            image = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container, "image", null)
-            name  = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container, "name", null)
+            image = lookup(container.value, "image", null)
+            name  = lookup(container.value, "name", null)
             dynamic "port" {
-              for_each = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container, "port", null) == null ? [] : [count.index]
+              for_each = lookup(container.value, "port", null) == null ? {} : { port : container.value.port }
               content {
-                container_port = local.k8s_deployments[count.index].deployment.spec.template.spec.container.port.container_port
-                host_ip        = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container.port, "host_ip", null)
-                host_port      = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container.port, "host_port", null)
-                name           = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container.port, "name", null)
-                protocol       = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container.port, "protocol", null)
+                container_port = port.value.container_port
+                host_ip        = lookup(port.value, "host_ip", null)
+                host_port      = lookup(port.value, "host_port", null)
+                name           = lookup(port.value, "name", null)
+                protocol       = lookup(port.value, "protocol", null)
               }
             }
             dynamic "resources" {
-              for_each = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container, "resources", null) == null ? [] : [count.index]
+              for_each = lookup(container.value, "resources", null) == null ? {} : { resources : container.value.resources }
               content {
                 dynamic "limits" {
-                  for_each = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container.resources, "limits", null) == null ? [] : [count.index]
+                  for_each = lookup(resources.value, "limits", null) == null ? {} : { limits : resources.value.limits }
                   content {
-                    cpu    = local.k8s_deployments[count.index].deployment.spec.template.spec.container.resources.limits.cpu
-                    memory = local.k8s_deployments[count.index].deployment.spec.template.spec.container.resources.limits.memory
+                    cpu    = limits.value.cpu
+                    memory = limits.value.memory
                   }
                 }
                 dynamic "requests" {
-                  for_each = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container.resources, "requests", null) == null ? [] : [count.index]
+                  for_each = lookup(resources.value, "requests", null) == null ? {} : { requests : resources.value.requests }
                   content {
-                    cpu    = local.k8s_deployments[count.index].deployment.spec.template.spec.container.resources.requests.cpu
-                    memory = local.k8s_deployments[count.index].deployment.spec.template.spec.container.resources.requests.memory
+                    cpu    = requests.value.cpu
+                    memory = requests.value.memory
                   }
                 }
               }
             }
             dynamic "liveness_probe" {
-              for_each = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container, "liveness_probe", null) == null ? [] : [count.index]
+              for_each = lookup(container.value, "liveness_probe", null) == null ? {} : { liveness_probe : container.value.liveness_probe }
               content {
+                initial_delay_seconds = lookup(liveness_probe.value, "initial_delay_seconds", null)
+                period_seconds        = lookup(liveness_probe.value, "period_seconds", null)
                 dynamic "http_get" {
-                  for_each = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container.liveness_probe, "http_get", null) == null ? [] : [count.index]
+                  for_each = lookup(liveness_probe.value, "http_get", null) == null ? {} : { http_get : liveness_probe.value.http_get }
                   content {
-                    path = local.k8s_deployments[count.index].deployment.spec.template.spec.container.liveness_probe.http_get.path
-                    port = local.k8s_deployments[count.index].deployment.spec.template.spec.container.liveness_probe.http_get.port
+                    path = http_get.value.path
+                    port = http_get.value.port
                     dynamic "http_header" {
-                      for_each = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container.liveness_probe.http_get, "http_header", null) == null ? [] : [count.index]
+                      for_each = lookup(http_get.value, "http_header", null) == null ? {} : { http_header : http_get.value.http.header }
                       content {
-                        name  = local.k8s_deployments[count.index].deployment.spec.template.spec.container.liveness_probe.http_get.http_header.name
-                        value = local.k8s_deployments[count.index].deployment.spec.template.spec.container.liveness_probe.http_get.http_header.value
+                        name  = http_header.value.name
+                        value = http_header.value.value
                       }
                     }
                   }
                 }
-                initial_delay_seconds = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container.liveness_probe, "initial_delay_seconds", null)
-                period_seconds        = lookup(local.k8s_deployments[count.index].deployment.spec.template.spec.container.liveness_probe, "period_seconds", null)
               }
             }
           }
@@ -155,24 +155,25 @@ resource "kubernetes_deployment" "self" {
 }
 
 resource "kubernetes_service" "self" {
-  count = length(local.k8s_services)
+  for_each = local.k8s_services
   metadata {
-    annotations   = lookup(local.k8s_services[count.index].service.metadata, "annotations", {})
-    generate_name = lookup(local.k8s_services[count.index].service.metadata, "name", null) == null ? lookup(local.k8s_deployments[count.index].deployment.metadata, "generate_name", {}) : null
-    name          = lookup(local.k8s_services[count.index].service.metadata, "name", null)
-    labels        = lookup(local.k8s_services[count.index].service.metadata, "labels", {})
-    namespace     = lookup(local.k8s_services[count.index].service.metadata, "namespace", null)
+    annotations   = lookup(each.value.service.metadata, "annotations", {})
+    generate_name = lookup(each.value.service.metadata, "name", null) == null ? lookup(each.value.service.metadata, "generate_name", null) : null
+    name          = lookup(each.value.service.metadata, "name", null)
+    labels        = lookup(each.value.service.metadata, "labels", {})
+    namespace     = lookup(each.value.service.metadata, "namespace", null)
   }
-
   spec {
-    selector = lookup(local.k8s_services[count.index].service.spec, "selector", {})
+    cluster_ip   = lookup(each.value.service.spec, "cluster_ip", null)
+    external_ips = lookup(each.value.service.spec, "external_ips", null)
+    selector     = lookup(each.value.service.spec, "selector", {})
     port {
-      name        = lookup(local.k8s_services[count.index].service.spec.port, "name", null)
-      node_port   = lookup(local.k8s_services[count.index].service.spec.port, "node_port", null)
-      port        = local.k8s_services[count.index].service.spec.port.port
-      protocol    = lookup(local.k8s_services[count.index].service.spec.port, "protocol", null)
-      target_port = lookup(local.k8s_services[count.index].service.spec.port, "target_port", null)
+      name        = lookup(each.value.service.spec.port, "name", null)
+      node_port   = lookup(each.value.service.spec.port, "node_port", null)
+      port        = each.value.service.spec.port.port
+      protocol    = lookup(each.value.service.spec.port, "protocol", null)
+      target_port = lookup(each.value.service.spec.port, "target_port", null)
     }
-    type = lookup(local.k8s_services[count.index].service.spec, "type", null)
+    type = lookup(each.value.service.spec, "type", null)
   }
 }
