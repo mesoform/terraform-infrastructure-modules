3b0221024782eeecaf87944a0411cb2f285b28db
sautlan <sautlan@gmail.com>
2020-10-23 01:30:20 +0300

    kubernetes_locals.tf changed

--- mcp/kubernetes_locals.tf
+++ mcp/kubernetes_locals.tf
@@ -9,23 +9,22 @@ locals {
     basename(dirname(kube_file)) => { config_map : yamldecode(file(kube_file)) }
     if ! contains(split("/", kube_file), "terraform")
   }
-  k8s_config_map_data = { for key1, value1 in local.k8s_config_map :
-    key1 => merge({
-      for key2, value2 in lookup(local.k8s_config_map[key1].config_map, "data", {}) :
-      key2 => value2
+
+  k8s_config_map_data = { for app_name, config in local.k8s_config_map :
+    app_name => merge({
+      for data_key, data_value in lookup(local.k8s_config_map[app_name].config_map, "data", {}) :
+      data_key => data_value
       }, {
-      for val in lookup(local.k8s_config_map[key1].config_map, "data_file", {}) :
-      basename(val) => file(val)
+      for file in lookup(local.k8s_config_map[app_name].config_map, "data_file", {}) :
+      basename(file) => file(file)
     })
   }
-  k8s_config_map_binary_data = { for key1, value1 in local.k8s_config_map :
-    key1 => merge({
-      for key2, value2 in lookup(local.k8s_config_map[key1].config_map, "binary_data", {}) :
-      key2 => value2
-      }, {
-      for val in lookup(local.k8s_config_map[key1].config_map, "binary_file", {}) :
-      basename(val) => filebase64(val)
-    })
+
+  k8s_config_map_binary_data = { for app_name, config in local.k8s_config_map :
+    app_name => {
+      for file in lookup(local.k8s_config_map[app_name].config_map, "binary_file", {}) :
+      basename(file) => filebase64(file)
+    }
   }
 
   k8s_deployments_files = fileset(path.root, "../**/k8s_deployment.y{a,}ml")
@@ -42,13 +41,13 @@ locals {
     if ! contains(split("/", kube_file), "terraform")
   }
 
-  k8s_secret_data = { for key1, value1 in local.k8s_secret :
-    key1 => merge({
-      for key2, value2 in lookup(local.k8s_secret[key1].secret, "data", {}) :
-      key2 => value2
+  k8s_secret_data = { for app_name, config in local.k8s_secret :
+    app_name => merge({
+      for data_key, data_value in lookup(local.k8s_secret[app_name].secret, "data", {}) :
+      data_key => data_value
       }, {
-      for val in lookup(local.k8s_secret[key1].secret, "data_file", {}) :
-      basename(val) => file(val)
+      for file in lookup(local.k8s_secret[app_name].secret, "data_file", {}) :
+      basename(file) => file(file)
     })
   }
 
@@ -84,6 +83,20 @@ locals {
   k8s_job = {
     for kube_file in local.k8s_job_files :
     basename(dirname(kube_file)) => { job : yamldecode(file(kube_file)) }
+    if ! contains(split("/", kube_file), "terraform")
+  }
+
+  k8s_cron_job_files = fileset(path.root, "../**/k8s_cron_job.y{a,}ml")
+  k8s_cron_job = {
+    for kube_file in local.k8s_cron_job_files :
+    basename(dirname(kube_file)) => { cron_job : yamldecode(file(kube_file)) }
+    if ! contains(split("/", kube_file), "terraform")
+  }
+
+  k8s_pod_autoscaler_files = fileset(path.root, "../**/k8s_pod_autoscaler.y{a,}ml")
+  k8s_pod_autoscaler = {
+    for kube_file in local.k8s_pod_autoscaler_files :
+    basename(dirname(kube_file)) => { pod_autoscaler : yamldecode(file(kube_file)) }
     if ! contains(split("/", kube_file), "terraform")
   }
 }
