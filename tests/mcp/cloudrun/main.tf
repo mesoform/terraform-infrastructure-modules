308613cf56008d216a7b87a5c615e1cc8545d60c
Alex Bailey <70575006+AlexBMes@users.noreply.github.com>
2020-10-29 13:09:00 +0000

    MCP-76 Fix traffic unit test

--- tests/mcp/cloudrun/main.tf
+++ tests/mcp/cloudrun/main.tf
@@ -36,15 +36,14 @@ output test_traffic_config {
   value = data.external.test_traffic_with_revision.result
 }
 
-////tests traffic block with no settings, run `terraform apply -var 'gcp_cloudrun_yml=resources/empty_traffic.yml'
-//data external test_traffic_empty{
-//  query = local.cloudrun_traffic["default"] == [] ? {} : {"format" = "not empty list"}
-//  program = ["python", "${path.module}/test_traffic_empty.py"]
-//}
-//
-//output test_traffic_empty {
-//  value = data.external.test_traffic_empty.result
-//}
+data external test_traffic_empty{
+  query = local.cloudrun_traffic["empty"] == [] ? {} : {"format" = "not empty list"}
+  program = ["python", "${path.module}/test_traffic_empty.py"]
+}
+
+output test_traffic_empty {
+  value = data.external.test_traffic_empty.result
+}
 
 
 
