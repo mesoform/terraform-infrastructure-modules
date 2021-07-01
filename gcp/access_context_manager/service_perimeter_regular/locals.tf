8a5a1ba4d57ef62777ce72f612e769183b373faa
danperezsan <dan@mesoform.com>
2021-07-01 15:08:25 +0200

    INFRA-39: Update vpc_accessible_services local var conditions

--- gcp/access_context_manager/service_perimeter_regular/locals.tf
+++ gcp/access_context_manager/service_perimeter_regular/locals.tf
@@ -11,7 +11,7 @@ locals {
   egress_policies  = lookup(local.egress_status, "egressPolicies", [])
   egress_resources = lookup(local.egress_status, "resources", {})
 
-  vpc_accessible_services = var.vpc_accessible_services == null ? ["RESTRICTED-SERVICES"] : var.vpc_accessible_services
+  vpc_accessible_services = var.vpc_accessible_services == null || var.vpc_accessible_services == [] ? ["RESTRICTED-SERVICES"] : var.vpc_accessible_services
   vpc_sc_supported_services = [
                                 "accessapproval.googleapis.com",
                                 "adsdatahub.googleapis.com",
