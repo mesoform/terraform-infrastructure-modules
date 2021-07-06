2f05f05262fb4bef5c2a2e75d4b3fd5717eda018
danperezsan <dan@mesoform.com>
2021-07-06 17:06:25 +0200

    INFRA-44: Remove locals

--- gcp/access_context_manager/access_level_manager/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  allowed_ip_subnetworks = var.allowed_ip_subnetworks == null ? ["DENY-ALL"] : var.allowed_ip_subnetworks
-  allowed_members = var.allowed_members == null ? ["DENY-ALL"] : var.allowed_members
-  allowed_regions = var.allowed_regions == null ? ["DENY-ALL"] : var.allowed_regions
-}
