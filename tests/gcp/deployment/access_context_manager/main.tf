b40dd5c71d8c69a08b70152d3e54a4170279813f
Alex Bailey <70575006+AlexBMes@users.noreply.github.com>
2021-09-07 16:19:52 +0100

    INFRA-45 fix invalid shortname

--- tests/gcp/deployment/access_context_manager/main.tf
+++ tests/gcp/deployment/access_context_manager/main.tf
@@ -1,6 +1,6 @@
 module regular_service_perimeter {
   source = "source-path"
-  name = "test-perimeter"
+  name = "test_perimeter"
   access_policy_name = "access-policy-name"
   ingress_file_path = "./ingressPolicies.yml"
   egress_file_path = "./egressPolicies.yml"
