477f5ea4da5a2b61215e7235b359451c25880035
Alex Bailey <alex.bailey@mesoform.com>
2022-05-06 11:08:08 +0100

    Stateful boot disk unit tests

--- tests/gcp/unit_tests/secure_swarm_node/main.tf
+++ tests/gcp/unit_tests/secure_swarm_node/main.tf
@@ -1,3 +1,8 @@
+terraform {
+  experiments = [module_variable_optional_attrs]
+}
+
+//noinspection HILUnresolvedReference
 data external test_blue_instance_template{
   query = {"security_level" = local.blue_instance_template.security_level}
   program = ["python", "${path.module}/test_blue_instance_template.py"]
@@ -7,6 +12,7 @@ output test_blue_instance_template {
   value = data.external.test_blue_instance_template.result
 }
 
+//noinspection HILUnresolvedReference
 data external test_green_instance_template{
   query = {"security_level" = local.blue_instance_template.security_level}
   program = ["python", "${path.module}/test_blue_instance_template.py"]
@@ -14,4 +20,14 @@ data external test_green_instance_template{
 
 output test_green_instance_template {
   value = data.external.test_green_instance_template.result
+}
+
+//noinspection HILUnresolvedReference
+data external test_stateful_boot_disk_config {
+  query = {"device_name" = local.stateful_boot_disk.0.device_name}
+  program = ["python", "${path.module}/test_stateful_boot_disk_config.py"]
+}
+
+output test_stateful_boot_disk_config {
+  value = data.external.test_stateful_boot_disk_config.result
 }
