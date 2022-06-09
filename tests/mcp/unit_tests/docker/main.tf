12d06560fc028fd681744a44bf390090ad8ef083
dgavrischuk <dgavrischuk@sciforce.solutions>
2022-06-09 13:08:24 +0300

    path to python changed

--- tests/mcp/unit_tests/docker/main.tf
+++ tests/mcp/unit_tests/docker/main.tf
@@ -9,7 +9,7 @@ output docker_config {
 
 data external docker_container {
   query   = local.docker_container["specs"].docker_container
-  program = ["/usr/local/bin/python3", "${path.module}/test_docker_container.py"]
+  program = ["python", "${path.module}/test_docker_container.py"]
 }
 output docker_container {
   value = data.external.docker_container.result
@@ -17,7 +17,7 @@ output docker_container {
 
 data external docker_image {
   query   = lookup(local.docker_image["specs"].docker_image.build, "build_arg", {})
-  program = ["/usr/local/bin/python3", "${path.module}/test_docker_image.py"]
+  program = ["python", "${path.module}/test_docker_image.py"]
 }
 output docker_image {
   value = data.external.docker_image.result
@@ -25,7 +25,7 @@ output docker_image {
 
 data external docker_network {
   query   = local.docker_network["specs"].docker_network
-  program = ["/usr/local/bin/python3", "${path.module}/test_docker_network.py"]
+  program = ["python", "${path.module}/test_docker_network.py"]
 }
 output docker_network {
   value = data.external.docker_network.result
@@ -33,7 +33,7 @@ output docker_network {
 
 data external docker_plugin {
   query   = lookup(local.docker_plugin["specs"], "docker_plugin", {})
-  program = ["/usr/local/bin/python3", "${path.module}/test_docker_plugin.py"]
+  program = ["python", "${path.module}/test_docker_plugin.py"]
 }
 output docker_plugin {
   value = data.external.docker_plugin.result
@@ -41,7 +41,7 @@ output docker_plugin {
 
 data external docker_registry_image {
   query   = local.docker_registry_image["specs"].docker_registry_image.build
-  program = ["/usr/local/bin/python3", "${path.module}/test_docker_registry_image.py"]
+  program = ["python", "${path.module}/test_docker_registry_image.py"]
 }
 output docker_registry_image {
   value = data.external.docker_registry_image.result
@@ -49,7 +49,7 @@ output docker_registry_image {
 
 data external docker_secret {
   query   = local.docker_secret["specs"].docker_secret
-  program = ["/usr/local/bin/python3", "${path.module}/test_docker_secret.py"]
+  program = ["python", "${path.module}/test_docker_secret.py"]
 }
 
 output docker_secret {
@@ -58,7 +58,7 @@ output docker_secret {
 
 data external docker_service {
   query   = lookup(local.docker_service["specs"].docker_service.endpoint_spec, "ports", {})
-  program = ["/usr/local/bin/python3", "${path.module}/test_docker_service.py"]
+  program = ["python", "${path.module}/test_docker_service.py"]
 }
 output docker_service {
   value = data.external.docker_service.result
@@ -66,7 +66,7 @@ output docker_service {
 
 data external docker_volume {
   query   = local.docker_volume["specs"].docker_volume
-  program = ["/usr/local/bin/python3", "${path.module}/test_docker_volume.py"]
+  program = ["python", "${path.module}/test_docker_volume.py"]
 }
 output docker_volume {
   value = data.external.docker_volume.result
