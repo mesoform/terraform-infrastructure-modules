e62ee67b5d8df6e191488839f1ee67635c0231fb
dan <dan@mesoform.com>
2022-09-01 18:06:36 +0200

    Added cloudrun_https_lb_serverless_neg to create NEG and HTTPS load balancer

--- gcp/compute_engine/cloudrun_https_lb_serverless_neg/variables.tf
+++ gcp/compute_engine/cloudrun_https_lb_serverless_neg/variables.tf
@@ -1,24 +1,29 @@
-variable "project_id" {
+variable cloudrun_lb_projectid {
   type = string
 }
 
-variable "region" {
+variable cloudrun_lb_region {
   description = "Location for load balancer and Cloud Run resources"
   default     = "europe-west2"
 }
 
-variable "ssl" {
+variable cloudrun_lb_ssl {
   description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
   type        = bool
   default     = true
 }
 
-variable "domain" {
+variable cloudrun_lb_domain {
   description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
   type        = string
 }
 
-variable "lb_name" {
+variable cloudrun_lb_name {
   description = "Name for load balancer and associated resources"
   default     = "tf-cr-lb"
+}
+
+variable cloudrun_service_name {
+  description = "Cloud Run service name"
+  type        = string
 }
