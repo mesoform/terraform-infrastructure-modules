cfa61149ccd755845b5eaefd900c9fbf05afa7e1
Alex Bailey <alex.bailey@mesoform.com>
2024-01-30 17:49:58 +0000

    Add outputs for debugging variables

--- gcp/access_context_manager/service_perimeter_regular/outputs.tf
+++ gcp/access_context_manager/service_perimeter_regular/outputs.tf
@@ -1,3 +1,10 @@
+output "var_ingress_policies_file_path" {
+  value = var.ingress_file_path
+}
+
+output "var_egress_policies_file_path" {
+  value = var.egress_file_path
+}
 output "locals_ingress_policies_file_path" {
   value = local.ingress_file
 }
