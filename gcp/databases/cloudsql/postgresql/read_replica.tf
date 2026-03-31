773510d21ede7843222788f12db172d5c80b6fcf
Alex Bailey <alex.bailey@mesoform.com>
2026-03-31 17:27:09 +0100

    Add option to postgres module to enable private access for google services

--- gcp/databases/cloudsql/postgresql/read_replica.tf
+++ gcp/databases/cloudsql/postgresql/read_replica.tf
@@ -40,7 +40,7 @@ resource "google_sql_database_instance" "replicas" {
         private_network    = lookup(ip_configuration.value, "private_network", null)
         require_ssl        = lookup(ip_configuration.value, "require_ssl", null)
         allocated_ip_range = lookup(ip_configuration.value, "allocated_ip_range", null)
-
+        enable_private_path_for_google_cloud_services = lookup(ip_configuration.value, "enable_private_path_for_google_cloud_services", null)
         dynamic "authorized_networks" {
           for_each = lookup(ip_configuration.value, "authorized_networks", [])
           content {
